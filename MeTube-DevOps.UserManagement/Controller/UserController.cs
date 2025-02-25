5b41ab3fe4aa7020345ccb150a1cfc1a7532071b
RonnieSamson <166392656+RonnieSamson@users.noreply.github.com>
2025-02-25 13:29:40 +0100

    added contoller

--- MeTube-DevOps.UserManagement/Controller/UserController.cs
+++ MeTube-DevOps.UserManagement/Controller/UserController.cs
@@ -1,25 +1,43 @@
-[HttpPost("signup")]
-public async Task<IActionResult> SignUp([FromBody] CreateUserDto request)
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using MeTube_DevOps.UserManagement.Repositories;
+using MeTube_DevOps.UserManagement.Data;
+
+
+namespace MeTube_DevOps.UserManagement.Controller
 {
-  if (!ModelState.IsValid)
+  [Route("api/[controller]")]
+  [ApiController]
+  public class UserController : ControllerBase
   {
-    return BadRequest(ModelState);
+
+
+    [HttpPost("signup")]
+    public async Task<IActionResult> SignUp([FromBody] CreateUserDto request)
+    {
+      if (!ModelState.IsValid)
+      {
+        return BadRequest(ModelState);
+      }
+
+      if (await _unitOfWork.Users.GetUserByUsernameAsync(request.Username) != null)
+      {
+        return BadRequest(new { Message = "Username already exists" });
+      }
+
+      if (await _unitOfWork.Users.GetUserByEmailAsync(request.Email) != null)
+      {
+        return BadRequest(new { Message = "Email already exists" });
+      }
+
+      var user = _mapper.Map<User>(request);
+
+      await _unitOfWork.Users.AddUserAsync(user);
+      await _unitOfWork.SaveChangesAsync();
+
+      return Ok(new { Message = "User signed up successfully" });
+    }
   }
-
-  if (await _unitOfWork.Users.GetUserByUsernameAsync(request.Username) != null)
-  {
-    return BadRequest(new { Message = "Username already exists" });
-  }
-
-  if (await _unitOfWork.Users.GetUserByEmailAsync(request.Email) != null)
-  {
-    return BadRequest(new { Message = "Email already exists" });
-  }
-
-  var user = _mapper.Map<User>(request);
-
-  await _unitOfWork.Users.AddUserAsync(user);
-  await _unitOfWork.SaveChangesAsync();
-
-  return Ok(new { Message = "User signed up successfully" });
+}
 }
