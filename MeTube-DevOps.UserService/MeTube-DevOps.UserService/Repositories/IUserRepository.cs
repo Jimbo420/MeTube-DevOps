01f8cadffb660d4a0bea905ca3713bb58dd79114
Sebastian Svensson <sebastian@rosmic.se>
2025-02-28 14:52:45 +0100

    refactor and adding tests

---
+++ MeTube-DevOps.UserService/MeTube-DevOps.UserService/Repositories/IUserRepository.cs
@@ -0,0 +1,20 @@
+using System;
+using MeTube_DevOps.UserService.Entities;
+
+namespace MeTube_DevOps.UserService.Repositories;
+
+public interface IUserRepository : IRepository<User>
+{
+    Task<IEnumerable<User>> GetAllUsersAsync();
+    Task<User?> GetUserByIdAsync(int id);
+    Task<User?> GetUserByEmailAsync(string email);
+    Task<bool> EmailExistsAsync(string email);
+    Task<bool> UsernameExistsAsync(string username);
+    Task<User?> GetUserByUsernameAsync(string username);
+    Task<int?> GetUserIdByEmailAsync(string email);
+    //Task<bool> ChangeUserRoleAsync(int userId, string newRole);
+    Task AddUserAsync(User user);
+    void UpdateUser(User user);
+    Task DeleteUser(User user);
+    // Task DeleteUserAsync(int userId);
+}
