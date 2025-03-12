8042dcf2f387e08fb82d89113367b7ea248a9711
Sebastian Svensson <sebastian@rosmic.se>
2025-03-12 16:12:04 +0100

    integration test works, must run UserService! hardcoded url is not ideal

--- MeTube-DevOps.UserService/MeTube-DevOps.UserService.IntegrTests/UserControllerTests.cs
+++ MeTube-DevOps.UserService/MeTube-DevOps.UserService.IntegrTests/UserControllerTests.cs
@@ -26,33 +26,38 @@ namespace MeTube_DevOps.UserService.IntegrationTests
         {
             // Arrange
             // Add some user data to the database
-            var expected1 = new User { Id = 1, Username = "user1", Email = "test1@mail.com", Password = "password1", Role = "User" };
-            var expected2 = new User { Id = 2, Username = "user2", Email = "test2@mail.com", Password = "password2", Role = "User" };
-            await _apiContext.PostAsync("/api/User/signup", new APIRequestContextOptions {DataObject = expected1});
-            await _apiContext.PostAsync("/api/User/signup", new APIRequestContextOptions {DataObject = expected2});
+            var expected1 = new User { Id = 666, Username = "user1", Email = "test1@mail.com", Password = "password1", Role = "User" };
+            var expected2 = new User { Id = 1337, Username = "user2", Email = "test2@mail.com", Password = "password2", Role = "User" };
+            await _apiContext.PostAsync("http://localhost:5218/api/User/signup", new APIRequestContextOptions {DataObject = expected1});
+            await _apiContext.PostAsync("http://localhost:5218/api/User/signup", new APIRequestContextOptions {DataObject = expected2});
 
             // Act
-            var response = await _apiContext.GetAsync("/api/User/manageUsers");
+            var response = await _apiContext.GetAsync("http://localhost:5218/api/User/manageUsers");
 
             // Assert
             response.Status.Should().Be(200);
 
-            var users = JsonSerializer.Deserialize<List<User>>(await response.TextAsync(), _serializerOptions);
+            // Change this line to deserialize to UserDto instead of User
+            var users = JsonSerializer.Deserialize<List<UserDto>>(await response.TextAsync(), _serializerOptions);
             users.Should().NotBeNull();
             ArgumentNullException.ThrowIfNull(users);
-            users.Should().BeOfType<List<User>>();
+            users.Should().BeOfType<List<UserDto>>();
 
             var actual1 = users.FirstOrDefault(u => u.Id == expected1.Id);
             actual1?.Id.Should().Be(expected1.Id);
             actual1?.Username.Should().Be(expected1.Username);
             actual1?.Email.Should().Be(expected1.Email);
-            actual1?.Role.Should().Be(expected1.Role);
+            
+            // Don't check for Role if it's not in the UserDto
+            // actual1?.Role.Should().Be(expected1.Role);
 
             var actual2 = users.FirstOrDefault(u => u.Id == expected2.Id);
             actual2?.Id.Should().Be(expected2.Id);
             actual2?.Username.Should().Be(expected2.Username);
             actual2?.Email.Should().Be(expected2.Email);
-            actual2?.Role.Should().Be(expected2.Role);
+            
+            // Don't check for Role if it's not in the UserDto
+            // actual2?.Role.Should().Be(expected2.Role);
 
             _output.WriteLine($"items: Count={users.Count}");
         }
