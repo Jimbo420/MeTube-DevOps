15236f39836b53815f738783edc4242ffa4d6478
Jimbo420 <66968943+Jimbo420@users.noreply.github.com>
2025-03-19 21:56:37 +0100

    Add initial Terraform configuration files for providers and variables

---
+++ terraform/providers.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.14.0"
+    }
+  }
+
+  required_version = ">= 1.10.3"
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = var.subscription_id
+}
