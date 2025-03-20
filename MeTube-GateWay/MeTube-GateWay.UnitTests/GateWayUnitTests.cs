c8f28003bef7bcb50fca8435226e3e43ef055aa9
Oskar Andreasson <159390304+HissAnd@users.noreply.github.com>
2025-03-20 21:48:32 +0100

    Test8

--- MeTube-GateWay/MeTube-GateWay.UnitTests/GateWayUnitTests.cs
+++ MeTube-GateWay/MeTube-GateWay.UnitTests/GateWayUnitTests.cs
@@ -1,7 +1,13 @@
+using Xunit;
+
 namespace MeTube_GateWay.UnitTests
 {
     public class GateWayUnitTests
     {
-
+        [Fact]
+        public void DummyTest()
+        {
+            Assert.True(true);
+        }
     }
 }
--- MeTube-GateWay/MeTube-Gateway/Program.cs
+++ MeTube-GateWay/MeTube-Gateway/Program.cs
@@ -3,7 +3,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-//Test6
+//Test8
 
 // Make sure the necessary environment variables are available.
 if (string.IsNullOrEmpty(Environment.GetEnvironmentVariable("METUBE_GATEWAY_PORT"))) {
