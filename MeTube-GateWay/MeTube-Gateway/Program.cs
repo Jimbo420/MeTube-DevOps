0b4667b4b88d047a79b136653c9087175dbd0fcf
Oskar Andreasson <159390304+HissAnd@users.noreply.github.com>
2025-03-20 22:00:12 +0100

    test 10

--- MeTube-GateWay/MeTube-Gateway/Program.cs
+++ MeTube-GateWay/MeTube-Gateway/Program.cs
@@ -3,7 +3,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-//Test9
+//Test10
 
 // Make sure the necessary environment variables are available.
 if (string.IsNullOrEmpty(Environment.GetEnvironmentVariable("METUBE_GATEWAY_PORT"))) {
